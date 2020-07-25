35089868dbd085fde3dabb6fcf4e24ee6b7bac02
Hugo Botelho <hugobotelho@gmail.com>
2020-07-25 18:25:50 +0100

    remove unnecessary files

--- app/run.r
+++
@@ -1,8 +0,0 @@
-source("functions.r")
-
-dependencies <- c("shiny", "shinyFiles", "shinyjs", 
-                  "ggplot2", "dplyr", "caTools", "raster",
-                  "parallel", "fs", "xlsx", "magick")
-sapply(dependencies, loadLibrary)
-
-runApp(launch.browser = TRUE)
